15c85e4130ce400b24e5c06993f294265e5d6c1a
marc-adaptive <marc.pinto@weareadaptive.com>
2025-10-09 19:57:23 -0400

    Setup index interface

--- artio-core/src/main/java/uk/co/real_logic/artio/engine/logger/Index.java
+++ artio-core/src/main/java/uk/co/real_logic/artio/engine/logger/Index.java
@@ -32,6 +32,10 @@ public interface Index extends FragmentHandler, AutoCloseable
         return getClass().getSimpleName();
     }
 
+    default void onStart()
+    {
+    }
+
     void close();
 
     /**
